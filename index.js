4d66339137523f55d931c1271c0f611b0451ee21
daniel <d.danielprince2003@gmail.com>
2024-06-07 11:59:10 +0530

    feat:Added Multer File Upload

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require("express");
 const cors = require("cors");
+const multer = require("multer");
 app = express();
 app.use(cors({ origin: "*" }));
-const PORT = 8000 | 3000
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "Books/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
 
+const upload = multer({ storage: storage });
+const PORT = 8000 || 3000;
 
-app.listen(PORT, ()=>{
-    console.log("PORT : " , PORT , " is running");
-})
+app.get("/", (req, res) => {
+  res.json({ request: "GET" });
+});
+
+app.post("/api/upload", upload.single("book"), (req, res) => {
+  res.status(200).json({ status: "Success", file: req.file });
+});
+
+app.use(express.static("./Books"));
+
+app.listen(PORT, () => {
+  console.log("PORT : ", PORT, " is running");
+});
