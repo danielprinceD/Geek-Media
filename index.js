5da73fca67177ecb0504a889adf9784b50348bcd
daniel <d.danielprince2003@gmail.com>
2024-06-07 13:24:17 +0530

    geek media app created

--- index.js
+++ index.js
@@ -2,22 +2,36 @@ const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
 const serveIndex = require("serve-index");
+const { default: axios } = require("axios");
 app = express();
 app.use(cors({ origin: "*" }));
 app.use("/Books", serveIndex("./Books"));
+const DB_URL = "http://localhost:3000/books";
 app.use("/Books", express.static("./Books"));
+const id =
+  new Date().getTime() +
+  new Date().getMinutes() +
+  new Date().getHours() +
+  new Date().getDate() +
+  new Date().getMonth() +
+  new Date().getFullYear();
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "Books/");
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname);
+    cb(null, id + "---" + file.originalname);
   },
 });
 const upload = multer({
   storage: storage,
   fileFilter: function (req, file, cb) {
-    if (file.mimetype == "application/pdf") cb(null, true);
+    if (
+      file.mimetype == "application/pdf" &&
+      req.body.name !== "" &&
+      req.body.subject !== ""
+    )
+      cb(null, true);
     else cb(null, false);
   },
 });
@@ -30,9 +44,21 @@ app.get("/api/upload", (req, res) => {
   res.sendFile("Books/");
 });
 
+app.get("/api/books", async (req, res) => {
+  const books = await axios.get(DB_URL);
+  res.json(books.data);
+});
+
 app.post("/api/upload", upload.single("book"), (req, res) => {
-  if (req.file) res.status(200).json({ status: "Success", file: req.file });
-  else res.status(404).json({ status: "File not accepted", file: req.file });
+  if (req.file && req.body.name && req.body.subject) {
+    axios.post(DB_URL, {
+      bookname: req.body.name,
+      filename: req.file.filename,
+      subject: req.body.subject,
+      location: req.file.path,
+    });
+    res.status(200).json({ status: "Success", file: req.file });
+  } else res.status(404).json({ status: "Error" });
 });
 
 app.listen(PORT, () => {
