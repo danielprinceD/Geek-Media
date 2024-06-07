fad57acdd5559ce3a686433a392a5aef156634b7
daniel <d.danielprince2003@gmail.com>
2024-06-07 12:25:36 +0530

    feat:Completed Server Index

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
+const serveIndex = require("serve-index");
 app = express();
 app.use(cors({ origin: "*" }));
+app.use("/Books", serveIndex("./Books"));
+app.use("/Books", express.static("./Books"));
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "Books/");
@@ -23,13 +26,14 @@ const PORT = 8000 || 3000;
 app.get("/", (req, res) => {
   res.json({ request: "GET" });
 });
+app.get("/api/upload", (req, res) => {
+  res.sendFile("Books/");
+});
 
 app.post("/api/upload", upload.single("book"), (req, res) => {
   if (req.file) res.status(200).json({ status: "Success", file: req.file });
   else res.status(404).json({ status: "File not accepted", file: req.file });
 });
-
-app.use(express.static("./Books"));
 
 app.listen(PORT, () => {
   console.log("PORT : ", PORT, " is running");
