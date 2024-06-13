9e0bae8a0ae9e6fc8a497cdc0996b6f290184a59
daniel <d.danielprince2003@gmail.com>
2024-06-13 19:51:17 +0530

    feat:App Version 2

--- app/src/main/java/com/example/geekmediaapp/Retrofit/RetrofitInstance.kt
+++ app/src/main/java/com/example/geekmediaapp/Retrofit/RetrofitInstance.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitInstance {
     companion object {
-        const val BASEURL = "https://d0c0-2409-40f4-103d-fd7f-861-b1ff-feab-98b1.ngrok-free.app"
+        const val BASEURL = "https://sour-bugs-go.loca.lt"
         fun getInstance() : Retrofit{
             return Retrofit.Builder()
                 .baseUrl(BASEURL)
